c6e1835d38f537509eff10bcc070dd6eae8d282c
Jannis Lauerwald <jannis1lauerwald@gmail.com>
2023-07-10 21:44:28 +0200

    Added Random Library Test program
    Signed-off-by: Jannis Lauerwald <jannis1lauerwald@gmail.com>

---
+++ src/RandomLibraryTest.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+public class RandomLibraryTest {
+    public static void main(String[] args) {
+        Random random=new Random();
+        int randomint=random.nextInt(20);
+        System.out.println("Random int: "+randomint);
+        double randomdouble=random.nextDouble(20);
+        System.out.println("Random double: "+randomdouble);
+        float randomfloat=random.nextFloat(20);
+        System.out.println("Random float: "+randomfloat);
+        boolean randomboolean=random.nextBoolean();
+        System.out.println("Random boolean: "+randomboolean);
+    }
+
+}
